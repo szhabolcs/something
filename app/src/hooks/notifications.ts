680c164725faa470216fc888cd5baebdecea1685
Nagy Szabolcs <szabi.nagy.ns@gmail.com>
2024-04-09 02:07:43 +0300

    fix(app): Added additional fixes for expo notifications

--- app/src/hooks/notifications.ts
+++ app/src/hooks/notifications.ts
@@ -53,18 +53,20 @@ export const usePushNotifications = (): PushNotificationState => {
         return;
       }
 
+      if (Platform.OS === "android") {
+        await Notifications.setNotificationChannelAsync("default", {
+          name: "default",
+          importance: Notifications.AndroidImportance.MAX,
+        });
+
+        console.log('Set notification channel')
+      }
+
       token = await Notifications.getExpoPushTokenAsync({
         projectId: Constants.expoConfig?.extra?.eas.projectId,
       });
     } else {
       alert("Must be using a physical device for Push notifications");
-    }
-
-    if (Platform.OS === "android") {
-      Notifications.setNotificationChannelAsync("default", {
-        name: "default",
-        importance: Notifications.AndroidImportance.MAX,
-      });
     }
 
     return token;
@@ -74,6 +76,14 @@ export const usePushNotifications = (): PushNotificationState => {
     registerForPushNotificationsAsync().then((token) => {
       setExpoPushToken(token);
       console.log("Expo Push Token: ", token?.data);
+
+      Notifications.getLastNotificationResponseAsync().then((response) => {
+        console.log('Expo last notification response: ', response);
+        setLastNotificationResponse(response ?? undefined);
+        if (response?.notification.request.content.data) {
+          navigate("Camera", response?.notification.request.content.data);
+        }
+      });
     });
 
     notificationListener.current =
@@ -82,20 +92,18 @@ export const usePushNotifications = (): PushNotificationState => {
       });
 
     responseListener.current =
-      Notifications.addNotificationResponseReceivedListener((response) => {
+      Notifications.addNotificationResponseReceivedListener(async (response) => {
         console.log('Expo notification response: ', response);
         if (response?.notification.request.content.data) {
           navigate("Camera", response?.notification.request.content.data);
+        } else {
+          const _response = await Notifications.getLastNotificationResponseAsync();
+          console.log('Expo last notification _response: ', _response);
+          if (_response?.notification.request.content.data) {
+            navigate("Camera", _response?.notification.request.content.data);
+          }
         }
       });
-
-    Notifications.getLastNotificationResponseAsync().then((response) => {
-      console.log('Expo last notification response: ', response);
-      setLastNotificationResponse(response ?? undefined);
-      if (response?.notification.request.content.data) {
-        navigate("Camera", response?.notification.request.content.data);
-      }
-    });
 
     return () => {
       Notifications.removeNotificationSubscription(
