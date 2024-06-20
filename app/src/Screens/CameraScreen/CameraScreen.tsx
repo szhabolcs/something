2b9686bfa8a7d26f37b6fa6a05696851b6d85c39
Nagy Szabolcs <szabi.nagy.ns@gmail.com>
2024-06-20 17:59:32 +0300

    feat(app): Add reward page after completion

--- app/src/Screens/CameraScreen/CameraScreen.tsx
+++ app/src/Screens/CameraScreen/CameraScreen.tsx
@@ -9,6 +9,185 @@ import CameraRepository from '../../repositories/camera/CameraRepository';
 import { manipulateAsync, FlipType } from 'expo-image-manipulator';
 import MyButton from '../../components/molecules/MyButton';
 import { ALERT_TYPE, Toast } from 'react-native-alert-notification';
+import ApiService, { ApiResponse } from '../../services/ApiService';
+import Column from '../../components/atoms/Column';
+import * as Icons from 'react-native-feather';
+import Label from '../../components/atoms/Label';
+
+const api = new ApiService();
+
+function calculatePointPercentage(data: any) {
+  const percentage =
+    ((data.level.currentScore - data.level.currentLevel.minThreshold) /
+      (data.level.nextLevel.minThreshold - data.level.currentLevel.minThreshold)) *
+    100;
+  return percentage + 1;
+}
+
+const RewardDisplay = ({
+  reward,
+  navigation
+}: {
+  reward: ApiResponse<typeof api.client.images.upload.$post, 200>;
+  navigation: any;
+}) => {
+  // reward = {
+  //   points: [
+  //     {
+  //       value: 5,
+  //       reason: 'OFF_SCHEDULE'
+  //     },
+  //     {
+  //       value: 5,
+  //       reason: 'ON_SCHEDULE'
+  //     },
+  //     {
+  //       value: 5,
+  //       reason: 'STREAK_KEPT'
+  //     }
+  //   ],
+  //   streak: {
+  //     value: 1,
+  //     reset: true
+  //   },
+  //   badge: {
+  //     icon: 'CheckSquare',
+  //     name: 'Complete 3 Things',
+  //     description: 'Complete a thing! You can do this!'
+  //   },
+  //   level: {
+  //     currentLevel: {
+  //       name: 'Rookie',
+  //       minThreshold: 10
+  //     },
+  //     nextLevel: {
+  //       name: 'Master',
+  //       minThreshold: 999999
+  //     },
+  //     currentScore: 130
+  //   }
+  // };
+
+  const points = reward.points.reduce((sum, point) => sum + point.value, 0);
+  const readableReason = {
+    ['OFF_SCHEDULE']: 'being off schedule',
+    ['ON_SCHEDULE']: 'being on schedule',
+    ['STREAK_KEPT']: 'keeping your streak'
+  };
+  const Icon = reward.badge ? Icons[reward.badge.icon as keyof typeof Icons] : Icons['Activity'];
+
+  return (
+    <Column
+      styles={{
+        flex: 1,
+        paddingTop: 30,
+        paddingVertical: 20,
+        paddingHorizontal: 23,
+        justifyContent: 'space-between'
+      }}
+    >
+      <Column styles={{ justifyContent: 'center', alignItems: 'center' }}>
+        {/* POINTS */}
+        <Text style={{ fontSize: 16 }}>You just earned</Text>
+        <Text style={{ fontSize: 70 }}>{points}</Text>
+        <Text style={{ fontSize: 16 }}>points!</Text>
+
+        <Column styles={{ gap: 5, marginTop: 20 }}>
+          {reward.points.map((point, id) => (
+            <Row key={id} styles={{ backgroundColor: '#16a34a', padding: 8, borderRadius: 5 }}>
+              <Text style={{ color: 'white' }}>
+                <Text style={{ fontWeight: 'bold' }}>{point.value} </Text> for {readableReason[point.reason]}
+              </Text>
+            </Row>
+          ))}
+        </Column>
+
+        <Row styles={{ gap: 20 }}>
+          {/* STREAK */}
+          <Column styles={{ marginTop: 50 }}>
+            <Text style={{ fontSize: 16 }}>Your current streak is</Text>
+            <Text style={{ fontSize: 30, textAlign: 'center', marginVertical: 8 }}>{reward.streak.value}</Text>
+            {reward.streak.reset && <Text>ℹ️ Streak has been reset</Text>}
+          </Column>
+
+          {/* BADGE */}
+          {reward.badge && (
+            <Column styles={{ marginTop: 50 }}>
+              <Text style={{ fontSize: 16 }}>You've earned the badge</Text>
+              <Column
+                styles={{
+                  gap: 5,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  backgroundColor: '#f0f0f0',
+                  padding: 10,
+                  borderRadius: 10,
+                  borderColor: '#f0f0f0',
+                  borderWidth: 1,
+                  marginTop: 5
+                }}
+              >
+                <Icon color={'black'} />
+                <Label text={reward.badge.name} />
+              </Column>
+            </Column>
+          )}
+        </Row>
+
+        {/* LEVEL */}
+        <Row styles={{ justifyContent: 'space-between', width: '100%' }}>
+          <Text style={{ fontSize: 16, marginTop: 70 }}>Your level is</Text>
+          <Text style={{ fontSize: 16, marginTop: 70 }}>{reward.level.currentScore} points</Text>
+        </Row>
+        <Row
+          styles={{
+            alignItems: 'center',
+            gap: 5,
+            justifyContent: 'space-between',
+            width: '100%',
+            marginTop: 20
+          }}
+        >
+          <Row
+            styles={{
+              alignItems: 'center',
+              gap: 5
+            }}
+          >
+            <Text>
+              {reward.level.currentLevel.name} ({reward.level.currentLevel.minThreshold})
+            </Text>
+          </Row>
+          <Text>
+            {reward.level.nextLevel.name} ({reward.level.nextLevel.minThreshold})
+          </Text>
+        </Row>
+        <Column
+          styles={{
+            width: '100%',
+            height: 5,
+            marginTop: 5,
+            backgroundColor: '#f0f0f0',
+            borderRadius: 20,
+            alignItems: 'flex-start',
+            justifyContent: 'center'
+          }}
+        >
+          <Column
+            styles={{
+              height: '100%',
+              width: `${calculatePointPercentage(reward)}%`,
+              backgroundColor: '#16a34a',
+              borderRadius: 20
+            }}
+          />
+        </Column>
+      </Column>
+
+      <MyButton accent smalltext text="Neat!" onPress={() => navigation.navigate('Home')} />
+    </Column>
+  );
+};
 
 export default function CameraScreen({ route, navigation }: any) {
   const [permission, requestPermission] = useCameraPermissions();
@@ -16,6 +195,7 @@ export default function CameraScreen({ route, navigation }: any) {
   const [facing, setFacing] = useState<'front' | 'back'>('front');
   const [pauseImageCapture, setPauseImageCapture] = useState(false);
   const [capturedPicture, setCapturedPicture] = useState<CameraCapturedPicture | null>(null);
+  const [reward, setReward] = useState<ApiResponse<typeof api.client.images.upload.$post, 200> | null>(null);
 
   const { height, width } = Dimensions.get('window');
   const cameraWidth = 300;
@@ -69,17 +249,16 @@ export default function CameraScreen({ route, navigation }: any) {
       setPauseImageCapture(true);
       const repo = new CameraRepository();
       // Throws error
-      const rewards = await repo.uploadImage(capturedPicture!.uri!, route.params.uuid);
+      const _reward = await repo.uploadImage(capturedPicture!.uri!, route.params.uuid);
 
-      console.log('[CameraScreen] rewards ', JSON.stringify(rewards, null, 2));
+      console.log('[CameraScreen] reward ', JSON.stringify(_reward, null, 2));
       setCapturingImage(false);
+      setReward(_reward);
 
       Toast.show({
         type: ALERT_TYPE.SUCCESS,
         textBody: 'Thing captured! ✨'
       });
-
-      navigation.navigate('Home');
     } catch (error) {
       Toast.show({
         type: ALERT_TYPE.WARNING,
@@ -97,11 +276,15 @@ export default function CameraScreen({ route, navigation }: any) {
   if (!permission.granted) {
     // Camera permissions are not granted yet.
     return (
-      <View style={styles.container}>
+      <View style={[styles.container, { justifyContent: 'center', gap: 20 }]}>
         <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
         <MyButton text={'Grant permission'} onPress={requestPermission} accent />
       </View>
     );
+  }
+
+  if (reward) {
+    return <RewardDisplay reward={reward} navigation={navigation} />;
   }
 
   return (
@@ -166,7 +349,7 @@ export default function CameraScreen({ route, navigation }: any) {
           </TouchableOpacity>
         )}
       </Row>
-      <View style={{ marginBottom: 50 }}>
+      <View style={{ marginBottom: 50, alignItems: 'center' }}>
         <H3>
           Current <H3 accent>Thing</H3> being captured
         </H3>
